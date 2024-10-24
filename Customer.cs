aa18cdddd65f883ac89b86334825f975c9b5e539
csaszy <csaszy2006@gmail.com>
2024-10-24 20:05:07 +0200

    class definitions done

---
+++ Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop
+{
+    internal class Customer : Record
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Rank { get; set; }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop
+{
+    internal class Product : Record
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
