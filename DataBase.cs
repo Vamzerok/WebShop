268b0a6a333e91d0fda1e96be20ed52614199710
csaszy <csaszy2006@gmail.com>
2024-11-15 23:36:17 +0100

    can't do this anymore

---
+++ DataBase.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Security.Permissions;
+using Newtonsoft.Json.Linq;
+using System.CodeDom;
+
+namespace WebShop
+{
+    internal class DataBase
+    {
+        public enum Table
+        {
+            Orders,
+            Products,
+            Customers
+        }        
+
+        JsonSerializer serializer;
+        StreamReader sr;
+        string path;
+
+        public List<Order> Orders;
+        public List<Product> Products;
+        public List<Customer> Customers;
+
+        public DataBase(string path) 
+        {
+            serializer = new JsonSerializer();
+            sr = new StreamReader(path);
+            this.path = path;
+
+            Orders = new List<Order>();
+            Products = new List<Product>();
+            Customers = new List<Customer>();
+
+            Deserialize();
+        }
+
+/*        public List<T> Select<T>(Table from, Func<T,bool> where) where T : Record
+        {
+            switch (from)
+            {
+                case Table.Orders:
+                    return Orders.Where(where);
+                    break;
+                case Table.Products:
+                    break;
+                case Table.Customers:
+                    break;
+            }
+        }
+*/        
+
+        public List<string> GetKeys(Table table)
+        {
+            List<string> keys = new List<string>();
+            JObject jsonObject = JObject.Parse(File.ReadAllText(path));
+            switch (table)
+            {
+                case Table.Orders:
+                    keys = (jsonObject["rendeles"] as JObject).Properties().Select(p => p.Name).ToList();
+                    break;
+                case Table.Products:
+
+                    break;
+                case Table.Customers:
+                    JObject ugyfelObj = (JObject)jsonObject["ugyfel"];
+                    keys = ugyfelObj.Properties().Select(p => p.Name).ToList();
+                    break;
+            }
+            return keys;
+        }
+
+        private void Deserialize()
+        {
+            JObject file = JObject.Parse(sr.ReadToEnd());
+
+            //deserializing orders
+            List<JToken> orders = file["rendeles"].Children().ToList();
+            orders.ForEach(e => Orders.Add(e.ToObject<Order>()));
+
+            //deserializing products 
+            List<JToken> products = file["termek"].Children().ToList();
+            products.ForEach(e => Products.Add(e.ToObject<Product>()));
+
+            //deserializing customers 
+            List<JToken> customers = file["ugyfel"].Children().ToList();
+            customers.ForEach(e => Customers.Add(e.ToObject<Customer>()));
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,6 +18,8 @@ using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 using System.Collections;
 using Newtonsoft.Json.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 
 namespace WebShop
 {
@@ -26,34 +28,101 @@ namespace WebShop
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DataBase dataBase;
         public MainWindow()
         {
             InitializeComponent();
-            var product = new Product(0) {
-                megnevezes = "laptop",
-                ar = 100,
-                mennyiseg = 10
-            };
 
-            string json = JsonConvert.SerializeObject(product);
-            using (StreamWriter sw = new StreamWriter("test.json"))
-            using (StreamReader sr = new StreamReader("webshop.json"))
-            {
-                ITraceWriter traceWriter = new MemoryTraceWriter();
-                JsonSerializer serializer = new JsonSerializer();
-                JsonReader jsonReader = new JsonTextReader(sr);
-
-                serializer.TraceWriter = traceWriter;
-                FileFormat file = (FileFormat)serializer.Deserialize(jsonReader, typeof(FileFormat));
-
-                serializer.Serialize(sw, file);
-
-                File.WriteAllText("log.txt", traceWriter.ToString());
-
-
-            }
-            System.Windows.Application.Current.Shutdown();
+            dataBase = new DataBase("webshop.json");
+            LoadListView(DataBase.Table.Orders);
         }
+
+        //what desperation looks like
+        private void LoadListView(DataBase.Table table) 
+        {
+            listbox.Items.Clear();
+            switch (table)
+            {
+                case DataBase.Table.Orders:
+                    listbox_label.Content = "Rendelések";
+                    foreach(var r in dataBase.Orders)
+                    {
+                        var lbItem = new ListBoxItem();
+                        lbItem.Selected += (sender, e) => { LoadDetailsView(r); };
+                        lbItem.Tag = r;
+                        lbItem.Content = $"Rendelés id: {r.id}";
+                        listbox.Items.Add(lbItem);
+                    }
+                    break;
+                case DataBase.Table.Customers:
+                    listbox_label.Content = "Ügyfelek";
+                    foreach(var r in dataBase.Customers)
+                    {
+                        var lbItem = new ListBoxItem();
+                        lbItem.Selected += (sender, e) => { LoadDetailsView(r); };
+                        lbItem.Tag = r;
+                        lbItem.Content = $"{r.nev}";
+                        listbox.Items.Add(lbItem);
+                    }
+                    break;
+                case DataBase.Table.Products:
+                    listbox_label.Content = "Termékek";
+                    foreach(var r in dataBase.Products)
+                    {
+                        var lbItem = new ListBoxItem();
+                        lbItem.Selected += (sender, e) => { LoadDetailsView(r); };
+                        lbItem.Tag = r;
+                        lbItem.Content = $"{r.megnevezes}";
+                        listbox.Items.Add(lbItem);
+                    }
+                    break;
+            }
+        }
+
+        private void LoadDetailsView(Record r)
+        {
+            switch (r)
+            {
+                case Customer customer:
+                    var grid = new Grid();
+                    grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star)});
+                    grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star)});
+
+                    //tbMain
+                    var tbMain = new TextBlock() { Text = (r as Customer).nev};
+                    Grid.SetColumn(tbMain, 0);
+                    grid.Children.Add(tbMain);                    
+
+                    //other info
+                    foreach(var e in dataBase.GetKeys(DataBase.Table.Customers))
+
+                    detailsView.Children.Add(grid);
+                    break;
+                case Product product:
+                    // Handle Product-specific logic
+                    break;
+                case Order order:
+                    // Handle Order-specific logic
+                    break;
+                default:
+                    throw new InvalidOperationException("Unknown Record type.");
+            }
+        }
+        //---------- events ----------  
+        private void OrderClick(object sender, RoutedEventArgs e)
+        {
+            LoadListView(DataBase.Table.Orders);
+        }
+
+        private void ProductClick(object sender, RoutedEventArgs e)
+        {
+            LoadListView(DataBase.Table.Products);
+        }
+
+        private void CusotmerClick(object sender, RoutedEventArgs e)
+        {
+            LoadListView(DataBase.Table.Customers);
+        } 
 
         private void btn_Save_Click(object sender, RoutedEventArgs e)
         {
@@ -68,5 +137,7 @@ namespace WebShop
         {
 
         } 
+
+
     }
 }
